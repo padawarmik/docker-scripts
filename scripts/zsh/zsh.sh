dabb9de84f8d15eb1536053aa8e44ec39bded594
padawarmik <43675819+padawarmik@users.noreply.github.com>
2022-11-05 02:17:13 +0100

    Update zsh.sh

--- scripts/zsh/zsh.sh
+++ scripts/zsh/zsh.sh
@@ -16,17 +16,7 @@ apt update
 printf "ZSH and oh-my-zsh\n"
 apt install -y curl zsh git neofetch
 curl https://raw.githubusercontent.com/padawarmik/docker-scripts/main/scripts/zsh/.p10k.zsh -o ~/.p10k.zsh -s
-if [ -f $HOME/.zshrc ]
-then
-    read -p "Your .zshrc file will be replaced. Do you want to continue?[Yy/Nn] " -n 1 -r
-    printf "\n"
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        printf "Deleting\n"
-        rm ~/.zshrc -f
-        curl https://raw.githubusercontent.com/padawarmik/docker-scripts/main/scripts/zsh/.zshrc -o ~/.zshrc -s
-    fi
-fi
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 if [ ! -d "$HOME/.zplug" ]; then
     install_zplug
@@ -36,10 +26,16 @@ if [ $SHELL == "/usr/bin/zsh" ]
 then
     omz reload 
 fi
-grep -q "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" ~/.zshrc
-if [ $? -eq 1 ]
+gif [ -f $HOME/.zshrc ]
 then
-    echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc
+    read -p "Your .zshrc file will be replaced. Do you want to continue?[Yy/Nn] " -n 1 -r
+    printf "\n"
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        printf "Deleting\n"
+        rm ~/.zshrc -f
+        curl https://raw.githubusercontent.com/padawarmik/docker-scripts/main/scripts/zsh/.zshrc -o ~/.zshrc -s
+    fi
 fi
 printf "\n\n"
 printf "The work has been done, restart your terminal now to feel the power of the ZSH command prompt\n"

099b264b40176f6115f8ba784a6fa94bfc02df34
padawarmik <padawarmik@gmail.com>
2023-06-20 17:43:01 +0200

    add more tools to install

--- scripts/zsh/zsh.sh
+++ scripts/zsh/zsh.sh
@@ -18,7 +18,19 @@ then
   printf "Update of apt\n"
   sudo apt update
   printf "ZSH and oh-my-zsh\n"
-  sudo apt install -y curl zsh git neofetch fzf
+  sudo apt install -y curl zsh git neofetch fzf ca-certificates apt-transport-https
+  curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+  curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+  sudo apt-get update
+  sudo apt-get install -y kubectl helm
+  snap --version
+  if [[ $? -ne 0 ]]
+  then
+    apt install snapd -y
+  fi
+  sudo snap install k9s
 fi
 curl https://raw.githubusercontent.com/padawarmik/docker-scripts/main/scripts/zsh/.p10k.zsh -o $HOME/.p10k.zsh -s
 
